b1a9c8860755da8269f3a122201c52c8651e1e41
Axel Herman <a.herman@afelio.be>
2019-09-06 17:11:30 +0200

    ui-kit design wip

--- templates/ui-kit-afelio/app/app-routing.module.ts
+++ templates/ui-kit-afelio/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { ButtonComponent } from './components';
 const routes: Routes = [
   { path: 'home', component: HomeComponent, pathMatch: 'full', data: {category: 'Home', name: 'Home'} },
   { path: 'button', component: ButtonComponent, pathMatch: 'full', data: {category: 'Components', name: 'Buttons'} },
-  { path: 'grid', component: HomeComponent, pathMatch: 'full', data: {category: 'Layout', name: 'Grid'} },
+  // { path: 'grid', component: HomeComponent, pathMatch: 'full', data: {category: 'Layout', name: 'Grid'} },
   { path: '**', redirectTo: '/home' }
 ];
 
