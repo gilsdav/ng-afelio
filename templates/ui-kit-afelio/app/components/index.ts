a7d8dc9722558d7b62b91aa3742479076e89172b
Axel Herman <a.herman@afelio.be>
2019-09-16 17:22:39 +0200

    add properties margin & padding

--- templates/ui-kit-afelio/app/components/index.ts
+++ templates/ui-kit-afelio/app/components/index.ts
@@ -1,10 +1,16 @@
 import { ButtonComponent } from './button/button.component';
 import { FormsComponent } from './forms/forms.component';
+import { MarginComponent } from './margin/margin.component';
+import { PaddingComponent } from './padding/padding.component';
 
 export const components = [
   ButtonComponent,
-  FormsComponent
+  FormsComponent,
+  MarginComponent,
+  PaddingComponent
 ];
 
 export * from './button/button.component';
 export { FormsComponent } from './forms/forms.component';
+export { MarginComponent } from './margin/margin.component';
+export { PaddingComponent } from './padding/padding.component';
