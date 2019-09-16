a41e14b0b60eb3cf949ca2f5048b143632228413
Axel Herman <a.herman@afelio.be>
2019-09-16 12:24:50 +0200

    add forms style

--- templates/ui-kit-afelio/app/app-routing.module.ts
+++ templates/ui-kit-afelio/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ButtonComponent } from './components';
+import { ButtonComponent, FormsComponent } from './components';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent, pathMatch: 'full', data: {category: 'Home', name: 'Home'} },
   { path: 'button', component: ButtonComponent, pathMatch: 'full', data: {category: 'Components', name: 'Buttons'} },
+  { path: 'forms', component: FormsComponent, pathMatch: 'full', data: {category: 'Components', name: 'Forms'} },
   // { path: 'grid', component: HomeComponent, pathMatch: 'full', data: {category: 'Layout', name: 'Grid'} },
   { path: '**', redirectTo: '/home' }
 ];
