ddfd7853f6bcbd264f76ea61ec049a11c09f34f5
Axel Herman <a.herman@afelio.be>
2019-09-16 12:25:10 +0200

    Forms

--- templates/ui-kit-afelio/app/components/index.ts
+++ templates/ui-kit-afelio/app/components/index.ts
@@ -1,7 +1,10 @@
 import { ButtonComponent } from './button/button.component';
+import { FormsComponent } from './forms/forms.component';
 
 export const components = [
-    ButtonComponent
+  ButtonComponent,
+  FormsComponent
 ];
 
 export * from './button/button.component';
+export { FormsComponent } from './forms/forms.component';
