8413ec2067bd930205aa23164e29affc5657cbad
Axel Herman <a.herman@afelio.be>
2019-09-18 11:20:25 +0200

    add layout grid to ui-kit styleguid

--- templates/ui-kit-afelio/app/components/index.ts
+++ templates/ui-kit-afelio/app/components/index.ts
@@ -2,15 +2,18 @@ import { ButtonComponent } from './button/button.component';
 import { FormsComponent } from './forms/forms.component';
 import { MarginComponent } from './margin/margin.component';
 import { PaddingComponent } from './padding/padding.component';
+import { GridComponent } from './grid/grid.component';
 
 export const components = [
   ButtonComponent,
   FormsComponent,
   MarginComponent,
-  PaddingComponent
+  PaddingComponent,
+  GridComponent
 ];
 
 export * from './button/button.component';
 export { FormsComponent } from './forms/forms.component';
 export { MarginComponent } from './margin/margin.component';
 export { PaddingComponent } from './padding/padding.component';
+export { GridComponent } from './grid/grid.component';
