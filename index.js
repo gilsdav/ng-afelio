33650d36a8b28a9b5e71b04b4fa1ce03b693d09d
David Gilson <davgilson@live.fr>
2023-01-19 11:44:03 +0100

    fix plugin

--- index.js
+++ index.js
@@ -145,14 +145,14 @@ pluginCommand
   })
   .option('-h, --help', 'output help message')
   .option('-l, --list', 'get the list of plugins from specific repo')
-  .option('-i, --ignoredParts', 'comma separated list of part names to ignore. Example: search,pagniation')
+  .option('-i, --ignored-parts <ignoredParts>', 'comma separated list of part names to ignore. Example: search,pagniation')
   .option('-p, --path', 'The path to create the module, relative to project directory. Default: ./shared/modules')
   .allowUnknownOption()
   // .parse(process.argv)
   .action((repo, name, options, command) => {
     if (repo && name) {
-      const ignoredParts = options.ignoredParts ? [ 'ignoredParts=' + options.ignoredParts ] : [];
-      const path = options.ignoredParts ? [ 'path=' + options.path ] : [];
+      const ignoredParts = options.ignoredParts ? [ '--ignored-parts=' + options.ignoredParts ] : [];
+      const path = options.path ? [ '--path=' + options.path ] : [];
       generate(`private-plugin`, repo, [name, ...ignoredParts, ...path, ...getAllArgs(command, options.help)]).then(() => {
         process.exit();
       });
