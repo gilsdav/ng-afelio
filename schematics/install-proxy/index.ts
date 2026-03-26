f863818b3780ec7b5900eff6ddfd713f88d9becb
David Gilson <davgilson@live.fr>
2026-03-26 19:31:05 +0100

    update install docker and add install proxy

---
+++ schematics/install-proxy/index.ts
@@ -0,0 +1,61 @@
+import { join, Path, strings } from '@angular-devkit/core';
+import { Rule, SchematicsException, Tree, apply, branchAndMerge, chain, mergeWith, move, template, url } from '@angular-devkit/schematics';
+import { buildDefaultPath, getWorkspace, updateWorkspace, WorkspaceDefinition } from '@schematics/angular/utility/workspace';
+
+// import { appendIntoEnvironment } from '../util/environment';
+
+import { Schema as ProxyOptions } from './schema';
+
+function updateConfig(): Rule {
+    return updateWorkspace((workspace: WorkspaceDefinition) => {
+        for (const [, project] of workspace.projects) {
+            for (const [name, target] of project.targets) {
+                if (name === 'serve') {
+                    if (!target.options) {
+                        target.options = {};
+                    }
+                    target.options['proxyConfig'] = 'src/proxy.conf.json';
+                }
+            }
+        }
+        if (!workspace.extensions['cli']) {
+            workspace.extensions['cli'] = {};
+        }
+        (workspace.extensions['cli'] as any)['schematicCollections'] = [
+            'ng-afelio',
+            '@schematics/angular'
+        ];
+    });
+}
+
+
+export default function(options: ProxyOptions): Rule {
+    return async (host: Tree) => {
+        const workspace = await getWorkspace(host);
+        const project = workspace.projects.get(options.project);
+
+        let projectAppPath: string;
+        if (project) {
+            projectAppPath = buildDefaultPath(project);
+        } else {
+            throw new SchematicsException(`Project "${options.project}" not found.`);
+        }
+
+        const templateSource = apply(url('./files'), [
+            template({
+              ...strings,
+              ...options,
+            }),
+            move(join(projectAppPath as Path, '..')),
+        ]);
+
+        return chain([
+            branchAndMerge(
+                chain([
+                    mergeWith(templateSource),
+                    updateConfig()
+                ])
+            ),
+        ]);
+    };
+}
