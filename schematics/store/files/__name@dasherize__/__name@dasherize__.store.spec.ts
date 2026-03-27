7b6c49b28ecc78e58dbd273e799c6ecdb960e1b1
David Gilson <davgilson@live.fr>
2026-03-27 12:00:25 +0100

    update store gen

--- schematics/store/files/__name@dasherize__/__name@dasherize__.store.spec.ts
+++ schematics/store/files/__name@dasherize__/__name@dasherize__.store.spec.ts
@@ -1,20 +1,20 @@
-import { TestBed, waitForAsync } from '@angular/core/testing';
-import { NgxsModule, Store } from '@ngxs/store';
+import { TestBed } from '@angular/core/testing';
+import { provideStore, Store } from '@ngxs/store';
 import { <%= classify(name) %>State } from './<%= dasherize(name) %>.store';
 
 describe('<%= classify(name) %> state', () => {
     let store: Store;
 
-    beforeEach(waitForAsync(() => {
-        TestBed.configureTestingModule({
-            imports: [
-                NgxsModule.forRoot([<%= classify(name) %>State])
-            ],
-            providers: [
-            ]
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [provideStore([<%= classify(name) %>State])],
+            providers: [],
         }).compileComponents();
+
         store = TestBed.inject(Store);
-    }));
+    });
 
+    it('should be created', () => {
+        expect(store).toBeDefined();
+    });
 });
-
